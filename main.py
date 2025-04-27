58dcb9bb8cdd5d50f7ee3e46204c4db7180f844c
jorgesilva2407 <jorge.lima2407@gmail.com>
2025-04-27 10:04:13 -0300

    Remove unused set_ulimit import from utils in main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import logging
 from crawler import Crawler
 from datetime import datetime
 from logger import setup_logger
-from utils import parse_args, parse_seeds, set_ulimit
+from utils import parse_args, parse_seeds
 
 
 def main():
